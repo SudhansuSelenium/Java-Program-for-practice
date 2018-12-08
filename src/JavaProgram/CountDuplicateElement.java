4705a1a1a23e11508a8b8b8b54dde365b78af8c6
Your Name <you@example.com>
2018-12-08 11:42:14 +0530

    test

--- src/JavaProgram/CountDuplicateElement.java
+++ src/JavaProgram/CountDuplicateElement.java
@@ -1,5 +1,6 @@
 package JavaProgram;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -18,21 +19,40 @@ public class CountDuplicateElement {
 		int count = 0;
 		for (int i = 0; i < num.length; i++) {
 			for (int j = i + 1; j < num.length; j++) {
-				if (num[i] == num[j]) {
+				if (num[i].equals(num[j])) {
 					count = count + 1;
-					System.out.println(num[i]);
+					System.out.println("equal "+num[i]);
 				}
 			}
 		}
 		System.out.println(count);
-		//Take Set
-		//Add the array to Integer
-		//add the integer to set is not added i.e false
-		//print integer
+		// Take Set
+		// Add the array to Integer
+		// add the integer to set is not added i.e false
+		// print integer
 		Set<Integer> set = new HashSet<Integer>();
-		for(Integer it : num) {
-			if(set.add(it)==false) {
+		for (Integer it : num) {
+			if (set.add(it) == false) {
 				System.out.println(it);
+			}
+		}
+		// String duplicate and count using for loop and Set
+		String str = "test test1 test";
+		String[] strarr = str.split(" ");
+		Set<String> se = new HashSet<String>();
+		for (String s : strarr) {
+			if (se.add(s) == false) {
+				System.out.println(s);
+			}
+		}
+		String string = "this is test this is demo";
+		String[] stringarr = string.split(" ");
+		System.out.println(Arrays.toString(stringarr));
+		for (int ii = 0; ii < stringarr.length; ii++) {
+			for (int jj = ii + 1; jj < stringarr.length; jj++) {
+				if (stringarr[ii].equals(stringarr[jj])) {
+					System.out.println("duplicate in string" + stringarr[ii]);
+				}
 			}
 		}
 	}
